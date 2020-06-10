979f9ce3e3797387c9a47acada55c8d86bdb4eba
Gavin Quirk <gavinquirk@gmail.com>
2020-06-10 16:46:57 -0700

    js timers, text and class changes

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('#text');
+
+// Total time for animation in ms
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+  text.innerHTML = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breatheAnimation, totalTime);
